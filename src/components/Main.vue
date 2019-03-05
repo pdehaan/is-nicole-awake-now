dacde21d09ad2c7af75350c14438e8522a1f47db
Peter deHaan <peter@deseloper.com>
2019-03-05 09:52:05 -0800

    FIRST BLOOD

---
+++ src/components/Main.vue
@@ -0,0 +1,60 @@
+<template>
+  <main class="main">
+    <header>
+      <h1 v-resize-text>Is Nicole Awake Now?</h1>
+    </header>
+    <p
+      id="msg"
+      v-text="msg"
+      v-resize-text="{ ratio: 0.6, minFontSize: '60px', maxFontSize: '500px' }"
+    />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  computed: {
+    msg() {
+      const qs = new URL(document.location).searchParams;
+      const hour = qs.get("hour") || qs.get("h") || new Date().getHours();
+      // <= 4am
+      if (hour <= 4) {
+        return "No";
+      }
+      // <= 7am
+      if (hour <= 7) {
+        return "Probably";
+      }
+      // <= 3pm
+      if (hour <= 15) {
+        return "Yes";
+      }
+      // <= 5pm
+      if (hour <= 17) {
+        return "Yes, but probably in pajamas";
+      }
+      // <= 7pm
+      if (hour <= 19) {
+        return "Unlikely";
+      }
+      // >= 8pm
+      return "Zzz...";
+    }
+  }
+};
+</script>
+
+<style scoped>
+main {
+  font-family: "Comic Sans MS", cursive;
+}
+header h1 {
+  color: #c9f;
+  margin: 0;
+}
+p#msg {
+  margin: 0;
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
+import VueResizeText from "vue-resize-text";
+
 import App from "./App.vue";
 
+Vue.use(VueResizeText);
 Vue.config.productionTip = false;
 
 new Vue({
